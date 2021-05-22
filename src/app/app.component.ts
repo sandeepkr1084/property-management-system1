b7a96f4b832d2813748944c4f8018c9c5da13164
sandeep kumar saw <sandeepkr1084@gmail.com>
2021-05-22 23:47:54 +0530

    property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddPropertyComponent } from './add-property/add-property.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'property-management-system1';
+  constructor(
+    public dialog: MatDialog,
+    ){
+
+  }
+  properties = []
+
+  createProperty(name, description, size, unit){
+    let data = {
+      name: name,
+      description: description,
+      size: size,
+      unit: unit
+    }
+    return data;
+  }
+
+  addProperty(){
+    const dialogRef = this.dialog.open(AddPropertyComponent);
+    dialogRef.afterClosed().subscribe(
+      data => {
+        if(data){
+          console.log(data)
+          data = data.data;
+          let t = this.createProperty(data.get("name").value, data.get("description").value, data.get("size").value, data.get("unit").value)
+          this.properties.push(t)
+        }
+      }
+    )
+  }
+  deleteProperty(i){
+    for(let j=i;j<this.properties.length-1;j++){
+      this.properties[j] = this.properties[j+1]
+    }
+    this.properties.pop();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDialogModule } from '@angular/material/dialog';
+import { AddPropertyComponent } from './add-property/add-property.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddPropertyComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    NoopAnimationsModule,
+    MatDialogModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
